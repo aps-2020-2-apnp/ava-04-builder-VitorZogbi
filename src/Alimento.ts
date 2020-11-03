203beb44e0566d02f32fac3085cfaa0f5e4b6e88
VitorZogbi <zogbivitor@hotmail.com>
2020-11-03 18:40:17 -0300

    ok

--- src/Alimento.ts
+++ src/Alimento.ts
@@ -37,3 +37,73 @@ export class Alimento {
   }
 
 }
+
+// tslint:disable-next-line: max-classes-per-file
+export class AlimentoBuilder {
+
+  private _alimento: Alimento
+
+  constructor(){
+    this._alimento = new Alimento('Marmita', 400, 343.3, 52.1, 9.6, 25.3, 0.145, 0.3, 0, 5.5)
+  }
+
+  reset() {
+    this._alimento.constructor()
+  }
+
+  descricao(descricao: string): AlimentoBuilder{
+    this._alimento.descricao = descricao
+    return this
+  }
+
+  porcao(porcao: number): AlimentoBuilder {
+    this._alimento.porcao = porcao
+    return this
+  }
+
+  valorEnergetico(valorEnergetico: number): AlimentoBuilder {
+    this._alimento.valorEnergetico = valorEnergetico
+    return this
+  }
+
+  carboidratos(carboidratos: number): AlimentoBuilder {
+    this._alimento.carboidratos = carboidratos
+    return this
+  }
+
+
+  fibras(fibras: number): AlimentoBuilder{
+    this._alimento.fibras = fibras
+    return this
+  }
+
+  proteinas(proteinas: number): AlimentoBuilder {
+    this._alimento.proteinas = proteinas
+    return this
+  }
+
+  sodio(sodio: number): AlimentoBuilder {
+    this._alimento.sodio = sodio
+    return this
+  }
+
+  gorduraSaturadas(gordurasSaturadas: number): AlimentoBuilder {
+    this._alimento.gordurasSaturadas = gordurasSaturadas
+    return this
+  }
+
+  gordurasTrans(gordurasTrans: number): AlimentoBuilder{
+    this._alimento.gordurasTrans = gordurasTrans
+    return this
+  }
+
+  gordurasTotais(gordurasTotais: number): AlimentoBuilder{
+    this._alimento.gordurasTotais = gordurasTotais
+    return this
+  }
+
+  get(): Alimento{
+    return this._alimento
+  }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
-import { Alimento } from './Alimento'
+import { Alimento, AlimentoBuilder } from './Alimento'
 
 const cereal = new Alimento('Marmita', 400, 343.3, 52.1, 9.6, 25.3, 0.145, 0.3, 0, 5.5)
 
 console.log(cereal);
 console.log(`${cereal}`) // toString
+
+// builder
+const alimento = new AlimentoBuilder()
+const marmita = alimento.descricao('Marmitao').porcao(600).valorEnergetico(500).
+  carboidratos(100).proteinas(50).gordurasTotais(40.5).gorduraSaturadas(20.2).
+  gordurasTrans(0).fibras(5.5).sodio(1.4).get()
+
+console.log(marmita);
+console.log(`${marmita}`);
+
+
+
